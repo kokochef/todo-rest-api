afb6141e80f765b616d3c8ece72f06979b2539d7
Kaustabh <kakoty.kaustabh@gmail.com>
2018-06-24 22:03:54 +0530

    Test GET /todos added

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,12 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 describe('POST /todos', () => {
   
   beforeEach((done) => {
@@ -25,7 +31,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -49,4 +55,24 @@ describe('POST /todos', () => {
         }).catch((e) => done(e));
       });
   });
+});
+
+describe('GET /todos', () => {
+  
+  beforeEach((done) => {
+    Todo.remove({}).then(() => {
+      Todo.insertMany(todos);
+    }).then(() => done());
+  });
+
+  it('should get all todos', (done) => {
+    request(app)
+     .get('/todos')
+     .expect(200)
+     .expect((res) => {
+       expect(res.body.todos.length).toBe(2);
+     })
+     .end(done);
+  })
+
 });
